3999820f5bab5fed654212bc011e928e10a99eb9
SunhoYum <sunho98a@gmail.com>
2023-04-02 21:55:06 +0900

    Fighting!

--- app/src/main/java/com/example/paintapp/MainActivity.kt
+++ app/src/main/java/com/example/paintapp/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         var path = Path()
         var paintBrush = Paint()
     }
-
+    //hi poki
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
