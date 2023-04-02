dd698fe2daff26120b04462ec359a33da6c2bf10
poki <ohsang1219@naver.com>
2023-04-02 21:32:34 +0900

    paint

---
+++ app/src/main/java/com/example/paintapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.paintapp
+
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import com.example.paintapp.PaintView.Companion.colorList
+import com.example.paintapp.PaintView.Companion.currentBrush
+import com.example.paintapp.PaintView.Companion.pathList
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+        // accessible throughout the application
+        var path = Path()
+        var paintBrush = Paint()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val redBtn = findViewById<ImageButton>(R.id.redColor)
+        val blueBtn = findViewById<ImageButton>(R.id.blueColor)
+        val blackBtn = findViewById<ImageButton>(R.id.blackColor)
+        val eraser = findViewById<ImageButton>(R.id.whiteColor)
+
+        redBtn.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.RED
+            currentColor(paintBrush.color)
+        }
+        blueBtn.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.BLUE
+            currentColor(paintBrush.color)
+        }
+        blackBtn.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.BLACK
+            currentColor(paintBrush.color)
+        }
+        eraser.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+            pathList.clear()
+            colorList.clear()
+            path.reset()
+        }
+    }
+
+    private fun currentColor(color: Int){
+        currentBrush = color
+        path = Path()
+    }
+}
