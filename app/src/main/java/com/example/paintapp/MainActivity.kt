1c26bbadf47d15636cee0f9888d8e837d6e5aafa
SunhoYum <sunho98a@gmail.com>
2023-05-06 22:26:32 +0900

    addPDF button, internal storage connection

--- app/src/main/java/com/example/paintapp/MainActivity.kt
+++ app/src/main/java/com/example/paintapp/MainActivity.kt
@@ -13,6 +13,9 @@ import com.example.paintapp.PaintView.Companion.colorList
 import com.example.paintapp.PaintView.Companion.currentBrush
 import com.example.paintapp.PaintView.Companion.pathList
 import com.google.android.material.navigation.NavigationView
+import android.os.Environment
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var toolbar : Toolbar
@@ -20,6 +23,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout : DrawerLayout
 
     private lateinit var navigationView : NavigationView
+
+    private lateinit var btnAddPdf: Button
+
+    private lateinit var tvStorageInfo: TextView
+    fun checkInternalStorage() {
+        val state = Environment.getExternalStorageState()
+        if (Environment.MEDIA_MOUNTED == state) {
+            val root = Environment.getExternalStorageDirectory()
+            val totalSpace = root.totalSpace / (1024 * 1024)
+            val freeSpace = root.freeSpace / (1024 * 1024)
+            val usedSpace = totalSpace - freeSpace
+            // Do something with the storage information
+        } else {
+            // Handle the case where external storage is not available
+        }
+    }
     companion object{
         // accessible throughout the application
         var path = Path()
@@ -29,6 +48,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val checkButton = findViewById<Button>(R.id.btnAddPdf)
+        checkButton.setOnClickListener {
+            checkInternalStorage()
+        }
 
         supportActionBar?.hide()
 
@@ -62,18 +86,18 @@ class MainActivity : AppCompatActivity() {
         toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+//      drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
 
         navigationView = findViewById<NavigationView>(R.id.navigationView)
 
         navigationView.setNavigationItemSelectedListener { menuItem ->
             // Handle navigation item click here
             when (menuItem.itemId) {
-                R.id.nav_camera -> {
-                    // Handle the home action
-                    drawerLayout.closeDrawers()
-                    true
-                }
+//                R.id.nav_camera -> {
+//                    // Handle the home action
+//                    drawerLayout.closeDrawers()
+//                    true
+//                }
                 R.id.nav_photo -> {
                     // Handle the gallery action
                     drawerLayout.closeDrawers()
@@ -89,8 +113,13 @@ class MainActivity : AppCompatActivity() {
 
             true // Return true to indicate that the item was handled
         }
+        btnAddPdf = findViewById(R.id.btnAddPdf)
+        tvStorageInfo = findViewById(R.id.tvStorageInfo)
 
-
+        btnAddPdf.setOnClickListener {
+            val internalStoragePath = Environment.getExternalStorageDirectory().absolutePath
+            tvStorageInfo.text = "Internal Storage: $internalStoragePath"
+        }
     }
 
     private fun currentColor(color: Int){
