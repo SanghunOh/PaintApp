461e7637e405ab03402840ce375b6e0f0331fd0e
rnldu <rnldudns98a@naver.com>
2023-05-21 01:23:20 +0900

    ChatGPT Response Popup 창에 띄워지는 것까지 완료

---
+++ app/src/main/java/com/example/paintapp/ModelPopupActivity.kt
@@ -0,0 +1,41 @@
+package com.example.paintapp
+
+import android.app.Activity
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Display.Mode
+import android.view.Window
+import android.widget.ImageView
+import android.widget.TextView
+
+import org.w3c.dom.Text
+
+class ModelPopupActivity(private val context : AppCompatActivity) {
+
+    private val dlg = Dialog(context)
+    private lateinit var answer_text : TextView
+    private lateinit var closeBtn : ImageView
+    private lateinit var minimizeBtn : ImageView
+    private var isMinimized: Boolean = false
+
+
+
+
+
+    fun show(content : String) {
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
+        dlg.setContentView(R.layout.activity_model_popup)     //다이얼로그에 사용할 xml 파일을 불러옴
+        dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
+
+        answer_text = dlg.findViewById(R.id.answer_field_textview)
+        answer_text.text = content
+
+        closeBtn = dlg.findViewById(R.id.close)
+        closeBtn.setOnClickListener {
+            dlg.dismiss()
+        }
+        minimizeBtn = dlg.findViewById(R.id.minimize)
+        dlg.show()
+    }
+}
