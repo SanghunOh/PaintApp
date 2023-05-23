84959193dc90f046948e03463cc687a4760ebd4d
rnldu <rnldudns98a@naver.com>
2023-05-23 23:17:08 +0900

    사이드바 생성 완료

--- app/src/main/java/com/example/paintapp/MainActivity.kt
+++ app/src/main/java/com/example/paintapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.paintapp
 
 import android.app.Activity
 import android.content.Intent
+import android.database.Cursor
+import android.graphics.Bitmap
 import android.graphics.Paint
 import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
@@ -11,9 +13,17 @@ import com.google.android.material.navigation.NavigationView
 import android.widget.Button
 import android.widget.TextView
 import android.net.Uri
+import android.provider.OpenableColumns
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.ui.NavigationUI
 import com.example.paintapp.API.RetrofitInstance
 import com.example.paintapp.API.response.Message
 import com.pspdfkit.configuration.PdfConfiguration
@@ -30,17 +40,23 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 
 
+var pdflist = mutableListOf<PdfFragment>()
 const val PICK_PDF_FILE = 1001
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 //    private lateinit var toolbar : Toolbar
 
     private lateinit var drawerLayout : DrawerLayout
     private lateinit var navigationView : NavigationView
     private lateinit var btnAddPdf: Button
     private lateinit var answerBtn: Button
+    private lateinit var btnNavi: ImageButton
+    private lateinit var btnTemp: ImageButton
     private lateinit var tvStorageInfo: TextView
+    private lateinit var global_frag: PdfFragment
+    private lateinit var menu: Menu
+    private var pdf_count = 1
 
-    private lateinit var paintFragment: PaintFragment
+    private var paintFragment: PaintFragment = PaintFragment()
 
     companion object{
         // accessible throughout the application
@@ -52,66 +68,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ocrapi.OcrTest("KakaoTalk_Photo_2023-05-06-21-59-55.jpg")
-        supportActionBar?.hide()
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navi_main)
+        navigationView.setNavigationItemSelectedListener(this)
+        btnNavi = findViewById(R.id.btnNavi)
+
+        btnNavi.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
 
 
+        menu = navigationView.menu
+        menu.add(Menu.NONE, Menu.FIRST, Menu.NONE, "메인 메뉴")
+        displayView(menu.getItem(0))
 
-//        val selectBrush = findViewById<ImageView>(R.id.selectBrush)
-//        val brushBtnGroup = findViewById<LinearLayout>(R.id.brushGroup)
-//        val redBtn = findViewById<ImageButton>(R.id.redColor)
-//        val blueBtn = findViewById<ImageButton>(R.id.blueColor)
-//        val blackBtn = findViewById<ImageButton>(R.id.blackColor)
-//        val clearBtn = findViewById<ImageView>(R.id.clear)
-        val checkButton = findViewById<Button>(R.id.btnAddPdf)
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.frame_layout, PaintFragment())
-            .addToBackStack(null)
-            .commit()
-
-
-//        selectBrush.setOnClickListener {
-//            displaySelectBox = false
-//            isSelected = false
-//            selectedStroke.clear()
-//            if (selectMode) {
-//                selectMode = false
-//                brushBtnGroup.visibility = VISIBLE
-//            }
-//            else {
-//                selectMode = true
-//                brushBtnGroup.visibility = GONE
-//            }
-//        }
-//        redBtn.setOnClickListener {
-//            if (!selectMode) {
-//                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-//                currentBrush = Color.RED
-//                currentColor(currentBrush)
-//            }
-//        }
-//        blueBtn.setOnClickListener {
-//            if (!selectMode) {
-//                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-//                currentBrush = Color.BLUE
-//                currentColor(currentBrush)
-//            }
-//        }
-//        blackBtn.setOnClickListener {
-//            if (!selectMode) {
-//                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-//                currentBrush = Color.BLACK
-//                currentColor(currentBrush)
-//            }
-//        }
-//        clearBtn.setOnClickListener {
-//            if (!selectMode) {
-//                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-//                pathList.clear()
-//                path.reset()
-//            }
-//        }
 
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply{
             addCategory(Intent.CATEGORY_OPENABLE)
@@ -121,18 +92,7 @@ class MainActivity : AppCompatActivity() {
         btnAddPdf.setOnClickListener {
             startActivityForResult(intent, PICK_PDF_FILE)
         }
-        /*
-        answerBtn = findViewById(R.id.answerBtn)
-        answerBtn.setOnClickListener {
-            val Popup = ModelPopupActivity(this)
-            Popup.show("Diaglog Success!!")
-        }
-
-         */
-
     }
-
-
 
     override fun onActivityResult(requestCode:Int, resultCode:Int,resultData:Intent?){
         super.onActivityResult(requestCode, resultCode, resultData)
@@ -140,6 +100,7 @@ class MainActivity : AppCompatActivity() {
             resultData?.data?.also{uri->
 
                 val documentUri = Uri.parse(uri.toString())
+                Log.d("PDFURI", "${uri.toString()}")
                 val config = PdfConfiguration.Builder()
                     .scrollDirection(PageScrollDirection.VERTICAL)
                     .scrollMode(PageScrollMode.CONTINUOUS)
@@ -147,19 +108,19 @@ class MainActivity : AppCompatActivity() {
                     .build()
 
                 val frag = PdfFragment.newInstance(documentUri, config)
-                //val mytext = MyText()
-                //frag.addOnTextSelectionChangeListener(mytext)
-                //frag.addOnAnnotationCreationModeChangeListener(mytext)
 
 
-                val transaction = supportFragmentManager.beginTransaction()
-                transaction.add(R.id.frame_layout, frag)
-                transaction.addToBackStack("detail")
-                transaction.commit()
+                pdflist.add(frag)
+                menu.add(Menu.NONE, Menu.FIRST+pdf_count, Menu.NONE, getFileName(uri))
+                val item = menu.getItem(pdf_count)
+                pdf_count+=1
+                displayView(item)
+
 
                 answerBtn = findViewById(R.id.answerBtn)
                 answerBtn.setOnClickListener {
                     val p0 = frag.textSelection
+
                     if(p0 == null)
                         Toast.makeText(this, "PDF를 open하고 원하는 텍스트를 선택하세요!", Toast.LENGTH_LONG).show()
                     else {
@@ -187,12 +148,11 @@ class MainActivity : AppCompatActivity() {
                                 .post(requestBody)
                                 .build()
 
-                            okHttpClient.newCall(request).enqueue(object : Callback {
+                        okHttpClient.newCall(request).enqueue(object : Callback {
                             override fun onResponse(call: Call, response: Response) {
                                 //Log.i("MyErrorMSG", "onResponse: ${response.body.toString()}")
                                 val json_obj = JSONObject(response.body?.string())
                                 val json_array = json_obj.optJSONArray("choices")
-
                                 val json_text = json_array.getJSONObject(0).getString("message")
                                 val json_obj2 = JSONObject(json_text)
                                 val json_text2 = json_obj2.getString("content")
@@ -209,5 +169,54 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+    override fun onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers()
+        }
+        else{
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        displayView(item)
+        return false
+    }
+    fun displayView(item:MenuItem){
+        var frag:Fragment? = null
+        var idx= (item.itemId - Menu.FIRST)
+        if(idx == 0)
+            frag = paintFragment
+        else if(idx >= 1)
+            frag = pdflist[idx-1]
+        if(frag != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, frag)
+                .commit()
+        }
+        drawerLayout.closeDrawers()
+    }
+    fun getFileName(uri:Uri):String?{
+        var result: String? = null
+        if(uri.scheme == "content"){
+            val cursor: Cursor? = contentResolver.query(uri,null,null,null,null)
+            try {
+                if(cursor != null && cursor.moveToFirst()){
+                    val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                    if(nameIndex >= 0) {
+                        result =
+                            cursor.getString(nameIndex)
+                    }
+                }
+            } finally {
+                cursor?.close()
+            }
+        }
+        if(result == null){
+            return uri.lastPathSegment
+        }
+        return result
     }
 }
